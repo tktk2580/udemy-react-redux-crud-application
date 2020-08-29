3b5b71103999c06f821120edac0acea1723ee1c9
瀉村健 <tks@mba.local>
2020-08-29 20:39:12 +0900

    create state

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import reducer from './reducers'
 import App from './components/App';
 // import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer)
 
@@ -18,4 +18,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
